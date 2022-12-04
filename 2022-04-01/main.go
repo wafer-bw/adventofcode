f6db259c4edae9b1eef4df7eeb2ffb10f3336912
wafer-bw <ben.jz.wakeford@gmail.com>
2022-12-04 11:18:05 -0500

    solve 2022-04-01 & 2022-04-02

---
+++ 2022-04-01/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/wafer-bw/adventofcode/tools/pather"
+	"github.com/wafer-bw/adventofcode/tools/reader"
+)
+
+const (
+	puzzleID string = "2022-04"
+)
+
+func solve(lines []string) int {
+	count := 0
+
+	for _, ln := range lines {
+		pair := [][]int{}
+		pairParts := strings.Split(ln, ",")
+		for _, part := range pairParts {
+			rangeParts := strings.Split(part, "-")
+			begin, _ := strconv.Atoi(rangeParts[0])
+			end, _ := strconv.Atoi(rangeParts[1])
+			pair = append(pair, []int{begin, end})
+		}
+
+		if pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1] || pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1] {
+			count++
+		}
+	}
+
+	return count
+}
+
+func main() {
+	log.Println(solve(reader.Read(pather.Path(puzzleID, false, false))))
+}
---
+++ 2022-04-02/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/wafer-bw/adventofcode/tools/pather"
+	"github.com/wafer-bw/adventofcode/tools/reader"
+)
+
+const (
+	puzzleID string = "2022-04"
+)
+
+func solve(lines []string) int {
+	count := 0
+
+	for _, ln := range lines {
+		var overlaps bool
+		filter := map[int]struct{}{}
+		pairParts := strings.Split(ln, ",")
+		for _, part := range pairParts {
+			rangeParts := strings.Split(part, "-")
+			begin, _ := strconv.Atoi(rangeParts[0])
+			end, _ := strconv.Atoi(rangeParts[1])
+			filter, overlaps = hasOverlaps(filter, begin, end)
+			if overlaps {
+				overlaps = false
+				count++
+				break
+			}
+		}
+	}
+
+	return count
+}
+
+func main() {
+	log.Println(solve(reader.Read(pather.Path(puzzleID, false, false))))
+}
+
+func hasOverlaps(filter map[int]struct{}, s, e int) (map[int]struct{}, bool) {
+	for i := s; i <= e; i++ {
+		if _, ok := filter[i]; !ok {
+			filter[i] = struct{}{}
+		} else {
+			return nil, true
+		}
+	}
+	return filter, false
+}
