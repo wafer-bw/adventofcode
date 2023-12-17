8f0616187b6b9ba1168cb86f92639fd7f8c1c540
wafer-bw <ben.jz.wakeford@gmail.com>
2023-12-17 17:12:15 -0500

    Rename package

---
+++ tools/mth/m.go
@@ -0,0 +1,37 @@
+// Package mth provides common math algorithms.
+package mth
+
+// GreatestCommonDivisor returns the greatest common divisor of a and b.
+func GreatestCommonDivisor(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// LeastCommonMultiple returns the least common multiple of a and b.
+func LeastCommonMultiple(a, b int) int {
+	return a * b / GreatestCommonDivisor(a, b)
+}
+
+// Sum returns the sum of all [Numeric] elements in s.
+func Sum[T Numeric](s []T) T {
+	var sum T
+	if len(s) == 0 {
+		return sum
+	}
+
+	for _, v := range s {
+		sum += v
+	}
+
+	return sum
+}
+
+// Numeric constraints a type to be a number which can be used for specific
+// math operations.
+type Numeric interface {
+	int | int32 | int64 | int16 | int8 | float32 | float64
+}
