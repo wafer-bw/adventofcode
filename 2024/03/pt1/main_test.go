865ee1eaf43d3d59ddbd874f0aaf47e4f069c350
Ben Wakeford <ben.jz.wakeford@gmail.com>
2024-12-03 16:35:33 -0500

    Solve 2024/03

---
+++ 2024/03/pt1/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSolve(t *testing.T) {
+	t.Parallel()
+
+	t.Run("solve correctly using sample input", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, 161, Solve(SampleInput))
+	})
+	t.Run("solve correctly using full input", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, 166630675, Solve(FullInput))
+	})
+}
---
+++ 2024/03/pt2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	//go:embed input-sample.txt
+	SampleInput string
+	//go:embed input.txt
+	FullInput string
+)
+
+var multPattern = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+
+func Solve(input string) int {
+	s := 0
+
+	data := strings.Join(strings.Split(input, "\n"), " ")
+	doSections := strings.Split(data, "do()")
+	for _, section := range doSections {
+		todoSection := strings.Split(section, "don't()")[0]
+		matches := multPattern.FindAllStringSubmatch(todoSection, -1)
+		for _, match := range matches {
+			if len(match) == 3 {
+				a := match[1]
+				b := match[2]
+				aInt, _ := strconv.Atoi(a)
+				bInt, _ := strconv.Atoi(b)
+				s += aInt * bInt
+			}
+		}
+	}
+
+	return s
+}
+
+func main() {
+	log.Printf("sample: %d", Solve(SampleInput))
+	log.Printf("full: %d", Solve(FullInput))
+}
